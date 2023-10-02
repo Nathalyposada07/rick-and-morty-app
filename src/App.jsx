9cb296d0c0157dfdb78ca3dc6e8b74e7e10f4676
Nathaly Posada <99698453+Nathalyposada07@users.noreply.github.com>
2023-10-01 22:18:30 -0500

    commit-1

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import './App.css'
+import Random from './components/Random'
+import logo from '/public/Rick-And-Morty-Logo.png'
+import video from '/public/videoplayback.mp4'
+
+
+
+
+function App() {
+  
+  // const [count, setCount] = useState (0)
+
+  return (
+
+    <div className='body'>
+
+      
+      <Random ></Random>
+      <footer className="footer">
+        <p className="footer__text">Creado por Nathaly Posada </p>
+      </footer>
+    </div>
+  )
+}
+
+export default App
